daf43ff8ace14e4520087b12946dbad0fadaf417
bochatayy <yannick.bochatay@meteo.fr>
2015-11-03 10:48:58 +0100

    Suppression des fonctions d'alignement (plugin spécifique Alignment).

--- JSYG.Container.js
+++ JSYG.Container.js
@@ -6,12 +6,12 @@
     if (typeof define != "undefined" && define.amd) define("jsyg-container",["jsyg","jsyg-path"],factory);
     else if (typeof JSYG != "undefined") {
         
-        if (JSYG.Path) factory(JSYG);
+        if (JSYG.Path) factory(JSYG,JSYG.Alignment);
         else throw new Error("JSYG.Path is needed");
     }
     else throw new Error("JSYG is needed");
     
-})(function(JSYG) {
+})(function(JSYG,Alignment) {
     
     "use strict";
     
@@ -33,7 +33,6 @@
     Container.prototype.onadditem = null;
     Container.prototype.onfreeitem = null;
     Container.prototype.onchange = null;
-    Container.prototype.onalign = null;
     
     Container.prototype.addItems = function(elmt) {
         
@@ -104,176 +103,7 @@
         
         return this;
     };
-    
-    Container.prototype.alignLeft = function() {
         
-        var left = this.getDim().x;
-        
-        this.children().each(function() {
-            var $this = new JSYG(this);
-            try { $this.setDim({x:left,from:$this}); }
-            catch(e) {}
-        });
-        
-        this.trigger('align');		
-        return this;
-    };
-    
-    Container.prototype.alignCenter = function() {
-        
-        var center = this.getCenter(),
-        that = this;
-        
-        this.children().each(function() {
-            
-            var $this = new JSYG(this);
-            
-            try {
-                
-                var mtx = $this.getMtx().inverse(),
-                dim = $this.getDim(),
-                dimP = $this.getDim(that),
-                pt1 = new JSYG.Vect(dimP.x+dimP.width/2,0).mtx(mtx),
-                pt2 = new JSYG.Vect(center.x,0).mtx(mtx);
-                
-                $this.setDim({
-                    x : dim.x + pt2.x - pt1.x,
-                    y : dim.y + pt2.y - pt1.y
-                });
-            }
-            catch(e) {}
-            
-        });
-        
-        this.trigger('align');	
-        
-        return this;
-    };
-    
-    Container.prototype.alignRight = function() {
-        
-        var dim = this.getDim(),
-        right = dim.x + dim.width,
-        that = this;
-        
-        this.children().each(function() {
-            
-            var $this = new JSYG(this);
-            
-            try {
-                
-                var mtx = $this.getMtx().inverse(),
-                dim = $this.getDim(),
-                dimP = $this.getDim(that),
-                pt1 = new JSYG.Vect(dimP.x,0).mtx(mtx),
-                pt2 = new JSYG.Vect(right - dimP.width,0).mtx(mtx);
-                
-                $this.setDim({
-                    x : dim.x + pt2.x - pt1.x,
-                    y : dim.y + pt2.y - pt1.y
-                });
-            }
-            catch(e) {}
-            
-        });
-        
-        this.trigger('align');
-        
-        return this;
-    };
-    
-    Container.prototype.alignTop = function() {
-        
-        var top = this.getDim().y,
-        that = this;
-        
-        this.children().each(function() {
-            
-            var $this = new JSYG(this);
-            
-            try {
-                var mtx = $this.getMtx().inverse(),
-                dim = $this.getDim(),
-                dimP = $this.getDim(that),
-                pt1 = new JSYG.Vect(0,dimP.y).mtx(mtx),
-                pt2 = new JSYG.Vect(0,top).mtx(mtx);
-                
-                $this.setDim({
-                    x : dim.x + pt2.x - pt1.x,
-                    y : dim.y + pt2.y - pt1.y
-                });
-            }
-            catch(e) {}
-            
-        });
-        
-        this.trigger('align');
-        
-        return this;
-    };
-    
-    Container.prototype.alignMiddle = function() {
-        
-        var center = this.getCenter(),
-        that = this;
-        
-        this.children().each(function() {
-            
-            var $this = new JSYG(this);
-            
-            try {
-                
-                var mtx = $this.getMtx().inverse(),
-                dim = $this.getDim(),
-                dimP = $this.getDim(that),
-                pt1 = new JSYG.Vect(0,dimP.y+dimP.height/2).mtx(mtx),
-                pt2 = new JSYG.Vect(0,center.y).mtx(mtx);
-                
-                $this.setDim({
-                    x : dim.x + pt2.x - pt1.x,
-                    y : dim.y + pt2.y - pt1.y
-                });
-            }
-            catch(e) {}
-            
-        });
-        
-        this.trigger('align');
-        
-        return this;
-    };
-    
-    Container.prototype.alignBottom = function() {
-        
-        var dim = this.getDim(),
-        bottom = dim.y + dim.height,
-        that = this;
-        
-        this.children().each(function() {
-            
-            var $this = new JSYG(this);
-            
-            try {
-                
-                var mtx = $this.getMtx().inverse(),
-                dim = $this.getDim(),
-                dimP = $this.getDim(that),
-                pt1 = new JSYG.Vect(0,dimP.y).mtx(mtx),
-                pt2 = new JSYG.Vect(0,bottom-dimP.height).mtx(mtx);
-                
-                $this.setDim({
-                    x : dim.x + pt2.x - pt1.x,
-                    y : dim.y + pt2.y - pt1.y
-                });
-            }
-            catch(e) {}
-            
-        });
-        
-        this.trigger('align');
-        
-        return this;
-    };
     
     JSYG.Container = Container;
     
